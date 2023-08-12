118ac22aae564d2af6967dc6b3bbe9e23c674953
Yash Nakadi <yashnakadi@gmail.com>
2023-08-12 17:50:56 -0400

    reorganizing code for efficiency

---
+++ bball_bigpicdash.py
@@ -0,0 +1,158 @@
+from dash import Dash, dcc, html
+from dash.dependencies import Input, Output, State
+import seaborn as sns
+import matplotlib.pyplot as plt
+import plotly.express as px
+import pandas as pd
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
+from plotly.offline import plot
+import numpy as np
+from bball_data_handling import data, teams, team_dict, team_color_map, team_names_df
+
+external_stylesheets = [
+    {
+        "href": (
+            "https://fonts.googleapis.com/css2?"
+            "family=Lato:wght@400;700&display=swap"
+        ),
+        "rel": "stylesheet",
+    },
+]
+app = Dash(__name__, external_stylesheets=external_stylesheets)
+app.title = "Baseball Analytics: Understand Your Team!"
+
+app.layout = html.Div(
+    children=[
+        html.Div(
+            children=[
+                html.P(children="⚾", className="header-emoji"),
+                html.H1(
+                    children="Baseball Analytics", className="header-title"
+                ),
+                html.P(
+                    children=(
+                        "EDA for baseball teams"
+                    ),
+                    className="header-description",
+                ),
+            ],
+            className="header",
+        ),
+        html.Div(
+            children=[
+                html.Div(
+                    children=[
+                        html.Div(children="All Teams", className="menu-title"),
+
+                        dcc.Checklist(
+                          id="my-checklist",
+                          options=team_dict,
+                          value=["NYA"],
+                          className='my_box_container',
+                          labelStyle={"display": "inline-block"},
+                        )
+                    ]
+                ),
+                html.Div(
+                    children=[
+                        html.Div(
+                            children="Date Range", className="menu-title"
+                        ),
+                        dcc.DatePickerRange(
+                            id="date-range",
+                            min_date_allowed=data["Date"].min().date(),
+                            max_date_allowed=data["Date"].max().date(),
+                            start_date=data["Date"].min().date(),
+                            end_date=data["Date"].max().date(),
+                        ),
+                    ]
+                ),
+                html.Div(
+                    children=[
+                        html.Div(
+                            children="Through N games", className="menu-title"
+                        ),
+                        dcc.Input(
+                            id='range',
+                            type='number',
+                            min=1,
+                            max=162,
+                            step=1
+                        ),
+                    ]
+                ),
+            ],
+            className="menu",
+        ),
+        html.Div(
+            children=[
+                html.Div(
+                    children=dcc.Graph(
+                        id="win-chart",
+                        config={"displayModeBar": False},
+                    ),
+                    className="card",
+                ),
+                html.Div(
+                    children=dcc.Graph(
+                        id="best-chart",
+                        config={"displayModeBar": False},
+                    ),
+                    className="card",
+                ),
+                html.Div(
+                    children=dcc.Graph(
+                        id="diff-chart",
+                        config={"displayModeBar": False},
+                    ),
+                    className="card",
+                )
+            ],
+            className="wrapper",
+        ),
+    ]
+)
+
+
+
+@app.callback(
+    Output("win-chart", "figure"),
+    Output("diff-chart", "figure"),
+    Output("best-chart","figure"),
+    Input("my-checklist","value"),
+    Input("date-range", "start_date"),
+    Input("date-range", "end_date"),
+    Input("range","value")
+)
+def update_charts(options, start_date, end_date, num_games):
+    filtered_data = data[(data['Team'].isin(options)) & (data['Date'] >= start_date) & (data['Date'] <= end_date)]
+    filtered_data['Game No.'] = list(pd.concat([pd.DataFrame(filtered_data[filtered_data["Team"] == team][['Team','Differential']].reset_index(drop=True).groupby(by="Team").cumsum().reset_index()['index']) for team in options])['index'])
+    filtered_data['Game No.'] = filtered_data['Game No.'] + 1
+    filtered_data["Year"] = filtered_data['Date'].dt.year
+    filtered_data["Wins"] = filtered_data[["Team","Result"]].groupby(by=["Team"]).cumsum()
+    filtered_data["Diff_Cum"] = filtered_data[["Team","Differential"]].groupby(by=["Team"]).cumsum()
+    win_chart_figure = px.line(filtered_data, x="Game No.", y="Wins", color="Team Name",
+                               title='Wins above or below 0.500', color_discrete_map=team_color_map)
+    diff_chart_figure = px.line(filtered_data, x="Game No.", y="Diff_Cum", color="Team",
+                                title="Team's Run Differential", color_discrete_map=team_color_map)
+    filtered_data = filtered_data.drop(columns=['Wins', 'Game No.'])
+    filtered_data['Game No.'] = list(pd.concat([pd.DataFrame(
+        filtered_data[(filtered_data["Team"] == team) & (filtered_data["Year"] == year)][
+            ['Team', 'Year', 'Differential']].reset_index(drop=True).groupby(
+            by=["Team", "Year"]).cumsum().reset_index()['index']) for team in options for year in range(1990, 2023)])['index'])
+    filtered_data2 = filtered_data[filtered_data['Game No.'] <= num_games]
+    filtered_data2["Wins"] = filtered_data2[["Team", "Year", "Result"]].groupby(by=["Team", "Year"]).cumsum()
+    filtered_data3 = filtered_data2[filtered_data2["Game No."] == num_games]
+    max_wins_df = filtered_data3[["Team", "Wins"]].groupby("Team").max().reset_index()  # .drop_duplicates()
+    semi_fin_df = pd.merge(filtered_data3[["Team", "Year", "Wins"]], max_wins_df, on=["Team", "Wins"]).groupby(
+        ["Team", "Wins"]).max().reset_index()[["Team", "Year"]]
+    fin_df = pd.merge(semi_fin_df, filtered_data2, how='left', on=["Team", "Year"])
+    fin_df["Team Name"] = fin_df['Year'].astype(str) + ' ' + fin_df['Team'].astype(str)
+
+    best_chart_figure = px.line(fin_df, x="Game No.", y="Wins", color="Team Name", color_discrete_map=team_color_map)
+    return win_chart_figure, diff_chart_figure, best_chart_figure
+
+
+if __name__ == "__main__":
+    app.run_server(debug=True)
--- bball_data_handling.py
+++ bball_data_handling.py
@@ -1,17 +1,9 @@
 
 # import packages needed for analysis
 
-from dash import Dash, dcc, html
-from dash.dependencies import Input, Output, State
+
 import pandas as pd
 import json
-import seaborn as sns
-import matplotlib.pyplot as plt
-import plotly.express as px
-from plotly.subplots import make_subplots
-import plotly.graph_objects as go
-from plotly.offline import plot
-import numpy as np
 from collections import Counter
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
@@ -69,14 +61,24 @@ for num in range(1990, 2023):
   exec(f"df_{num} = pd.read_csv('C:/Users/ynakadi/PycharmProjects/General Projects/Lib/retrosheets/gl{num}.txt')")
   exec(f"df_{num}.columns = all_columns")
   exec(f"df_{num}['Date'] =  pd.to_datetime(df_{num} ['Date'], format='%Y%m%d')")
+  exec(f"df_{num}['Game Type'] =  'Regular Season' ")
 
 
-df_2020
+for playoff_series in ["wc", "dv", "lc", "ws"]:
+  exec(f"df_{playoff_series} = pd.read_csv('C:/Users/ynakadi/PycharmProjects/General Projects/Lib/retrosheets/gl{playoff_series}.txt')")
+  exec(f"df_{playoff_series}.columns = all_columns")
+  exec(f"df_{playoff_series}['Date'] =  pd.to_datetime(df_{playoff_series} ['Date'], format='%Y%m%d')")
+  exec(f"df_{playoff_series}['Game Type'] =  'Playoffs' ")
+
 
 # Compose list of desired dataframes
 list_df = []
-exec("list_df = ["+ ",".join(['df_'+ str(i) for i in range(1990,2023,1)]) +"]")
-
+exec("list_df = ["+ ",".join(['df_'+ str(i) for i in range(1990,2023)]) +"]")
+print(list_df[-1])
+print('HI')
+for df in [df_wc, df_dv, df_lc, df_ws]:
+    list_df.append(df)
+print(list_df)
 df_total = pd.concat(list_df) # concatenate
 df_total = pd.DataFrame(df_total) # convert to pandas DataFrame
 df_total.shape # check the shape of large dataframe
@@ -109,7 +111,7 @@ home_features = ["Date", "Home Team", "Home Team League", "Home Score", "Length 
                  "Home Stolen Bases", "Home Caught Stealing", "Home Grounded into Double Plays", "Home Awarded First on Catcher's Interference",
                  "Home Left on Base", 'Home Pitchers Used', 'Home Individual Earned Runs', 'Home Team Earned Runs', 'Home Wild Pitches', 'Home Balks', 'Home Putouts',
                  'Home Assists', 'Home Errors', 'Home Passed Balls', 'Home Double Plays', 'Home Triple Plays',
-                 'Additional Information', 'Acquisition Information', 'Home Result', 'Home Team Game Number','Home Differential', 'Year','Month','Home Opponent Line Score', 'Home Opponent Score']
+                 'Additional Information', 'Acquisition Information', 'Home Result', 'Home Team Game Number','Home Differential', 'Year','Month','Home Opponent Line Score', 'Home Opponent Score', "Game Type"]
 
 # Define visiting team columns
 visiting_features = ["Date", "Visiting Team", "Visting Team League", "Visiting Score", "Length of Game in Outs",
@@ -119,7 +121,7 @@ visiting_features = ["Date", "Visiting Team", "Visting Team League", "Visiting S
                  "Visiting Stolen Bases", "Visiting Caught Stealing", "Visiting Grounded into Double Plays", "Visiting Awarded First on Catcher's Interference",
                  "Visiting Left on Base", 'Visiting Pitchers Used', 'Visiting Individual Earned Runs', 'Visiting Team Earned Runs', 'Visiting Wild Pitches', 'Visiting Balks', 'Visiting Putouts',
                  'Visiting Assists', 'Visiting Errors', 'Visiting Passed Balls', 'Visiting Double Plays', 'Visiting Triple Plays',
-                 'Additional Information', 'Acquisition Information', 'Visiting Result', 'Visiting Team Game Number','Visiting Differential', 'Year','Month','Visiting Opponent Line Score', 'Visiting Opponent Score']
+                 'Additional Information', 'Acquisition Information', 'Visiting Result', 'Visiting Team Game Number','Visiting Differential', 'Year','Month','Visiting Opponent Line Score', 'Visiting Opponent Score', "Game Type"]
 
 # Define cumulative dataset column headers
 columns = ["Date", "Team", "League", "Score", "Length of Game in Outs",
@@ -129,7 +131,7 @@ columns = ["Date", "Team", "League", "Score", "Length of Game in Outs",
                  "Stolen Bases", "Caught Stealing", "Grounded into Double Plays", "Awarded First on Catcher's Interference",
                  "Left on Base", 'Pitchers Used', 'Individual Earned Runs', 'Team Earned Runs', 'Wild Pitches', 'Balks', 'Putouts',
                  'Assists', 'Errors', 'Passed Balls', 'Double Plays', 'Triple Plays',
-                 'Additional Information', 'Acquisition Information', "Result", 'Team Game Number', 'Differential', 'Year','Month','Opponent Line Score', 'Opponent Score']
+                 'Additional Information', 'Acquisition Information', "Result", 'Team Game Number', 'Differential', 'Year','Month','Opponent Line Score', 'Opponent Score', "Game Type"]
 
 # Create home and visiting dataframes
 df_visiting = df_total[visiting_features]
@@ -144,19 +146,6 @@ df_final = pd.concat([df_home, df_visiting])
 df_final['Team_Fin'] = df_final['Year'].apply(lambda x: str(x)) + '_' + df_final['Team']
 df_final.sort_values(by = ['Team','Date'])
 
-df_trim = df_final[['Team_Fin','Result', 'Team Game Number','At-Bats','Double Plays','Differential','Score','Home Runs','Doubles','Hits']]
-
-
-df_teams = df_trim.groupby(by=['Team_Fin']).agg({'Result':'sum','Team Game Number': 'max', 'At-Bats':'sum','Differential':'sum','Score':'sum','Home Runs':'sum','Doubles':'sum'}).reset_index()
-df_teams['Win Percentage'] = df_teams['Result'] / df_teams['Team Game Number']
-df_team_bottom = df_teams.sort_values(by='Win Percentage').head(10)
-df_team_top = df_teams.sort_values(by='Win Percentage', ascending=False).head(10)
-df_teams_fin = pd.concat([df_team_top, df_team_bottom])
-df_teams_fin
-
-teams = set(df_teams_fin['Team_Fin'])
-teams
-df_trim
 
 team_names_df = pd.read_csv('C:/Users/ynakadi/PycharmProjects/General Projects/Lib/bballfiles/CurrentNames.csv')
 team_names_df#['Team Name'] = team_names_df['CITY'] + ' ' + team_names_df['NICKNAME']
@@ -170,13 +159,7 @@ team_dict = team_names_df[team_names_df['Former Name'].isin(name_change_dict.val
 df_final["Team Name"] = df_final["Team"].apply(lambda x: team_dict[x])
 len(set(df_final["Team Name"]))
 
-data = {'D':[2015,2015,2015,2015,2016,2016,2016,2017,2017,2017], 'Q':np.arange(10)}
-df = pd.DataFrame(data)
-df['Q_cum'] = df.groupby('D').cumsum()
-df
-data = df_final.sort_values(by=["Team","Date"])
-teams = data["Team"].sort_values().unique()
-team_dict
+
 team_color_map={
                 "Arizona Diamondbacks" : '#940818',
                 "Atlanta Braves":'#CF1010',
@@ -211,152 +194,6 @@ team_color_map={
 
                 }
 
-data = df_final.sort_values(by=["Team","Date"])
+data = df_final[df_final["Game Type"] == 'Regular Season'].sort_values(by=["Team","Date"])
 teams = data["Team"].sort_values().unique()
 
-external_stylesheets = [
-    {
-        "href": (
-            "https://fonts.googleapis.com/css2?"
-            "family=Lato:wght@400;700&display=swap"
-        ),
-        "rel": "stylesheet",
-    },
-]
-app = Dash(__name__, external_stylesheets=external_stylesheets)
-app.title = "Baseball Analytics: Understand Your Team!"
-
-app.layout = html.Div(
-    children=[
-        html.Div(
-            children=[
-                html.P(children="⚾", className="header-emoji"),
-                html.H1(
-                    children="Baseball Analytics", className="header-title"
-                ),
-                html.P(
-                    children=(
-                        "EDA for baseball teams"
-                    ),
-                    className="header-description",
-                ),
-            ],
-            className="header",
-        ),
-        html.Div(
-            children=[
-                html.Div(
-                    children=[
-                        html.Div(children="All Teams", className="menu-title"),
-
-                        dcc.Checklist(
-                          id="my-checklist",
-                          options=team_dict,
-                          value=["NYA"],
-                          className='my_box_container',
-                          labelStyle={"display": "inline-block"},
-                        )
-                    ]
-                ),
-                html.Div(
-                    children=[
-                        html.Div(
-                            children="Date Range", className="menu-title"
-                        ),
-                        dcc.DatePickerRange(
-                            id="date-range",
-                            min_date_allowed=data["Date"].min().date(),
-                            max_date_allowed=data["Date"].max().date(),
-                            start_date=data["Date"].min().date(),
-                            end_date=data["Date"].max().date(),
-                        ),
-                    ]
-                ),
-                html.Div(
-                    children=[
-                        html.Div(
-                            children="Through N games", className="menu-title"
-                        ),
-                        dcc.Input(
-                            id='range',
-                            type='number',
-                            min=1,
-                            max=162,
-                            step=1
-                        ),
-                    ]
-                ),
-            ],
-            className="menu",
-        ),
-        html.Div(
-            children=[
-                html.Div(
-                    children=dcc.Graph(
-                        id="win-chart",
-                        config={"displayModeBar": False},
-                    ),
-                    className="card",
-                ),
-                html.Div(
-                    children=dcc.Graph(
-                        id="best-chart",
-                        config={"displayModeBar": False},
-                    ),
-                    className="card",
-                ),
-                html.Div(
-                    children=dcc.Graph(
-                        id="diff-chart",
-                        config={"displayModeBar": False},
-                    ),
-                    className="card",
-                )
-            ],
-            className="wrapper",
-        ),
-    ]
-)
-
-
-
-@app.callback(
-    Output("win-chart", "figure"),
-    Output("diff-chart", "figure"),
-    Output("best-chart","figure"),
-    Input("my-checklist","value"),
-    Input("date-range", "start_date"),
-    Input("date-range", "end_date"),
-    Input("range","value")
-)
-def update_charts(options, start_date, end_date, num_games):
-    filtered_data = data[(data['Team'].isin(options)) & (data['Date'] >= start_date) & (data['Date'] <= end_date)]
-    filtered_data['Game No.'] = list(pd.concat([pd.DataFrame(filtered_data[filtered_data["Team"] == team][['Team','Differential']].reset_index(drop=True).groupby(by="Team").cumsum().reset_index()['index']) for team in options])['index'])
-    filtered_data['Game No.'] = filtered_data['Game No.'] + 1
-    filtered_data["Year"] = filtered_data['Date'].dt.year
-    filtered_data["Wins"] = filtered_data[["Team","Result"]].groupby(by=["Team"]).cumsum()
-    filtered_data["Diff_Cum"] = filtered_data[["Team","Differential"]].groupby(by=["Team"]).cumsum()
-    win_chart_figure = px.line(filtered_data, x="Game No.", y="Wins", color="Team Name",
-                               title='Wins above or below 0.500', color_discrete_map=team_color_map)
-    diff_chart_figure = px.line(filtered_data, x="Game No.", y="Diff_Cum", color="Team",
-                                title="Team's Run Differential", color_discrete_map=team_color_map)
-    filtered_data = filtered_data.drop(columns=['Wins', 'Game No.'])
-    filtered_data['Game No.'] = list(pd.concat([pd.DataFrame(
-        filtered_data[(filtered_data["Team"] == team) & (filtered_data["Year"] == year)][
-            ['Team', 'Year', 'Differential']].reset_index(drop=True).groupby(
-            by=["Team", "Year"]).cumsum().reset_index()['index']) for team in options for year in range(1990, 2023)])['index'])
-    filtered_data2 = filtered_data[filtered_data['Game No.'] <= num_games]
-    filtered_data2["Wins"] = filtered_data2[["Team", "Year", "Result"]].groupby(by=["Team", "Year"]).cumsum()
-    filtered_data3 = filtered_data2[filtered_data2["Game No."] == num_games]
-    max_wins_df = filtered_data3[["Team", "Wins"]].groupby("Team").max().reset_index()  # .drop_duplicates()
-    semi_fin_df = pd.merge(filtered_data3[["Team", "Year", "Wins"]], max_wins_df, on=["Team", "Wins"]).groupby(
-        ["Team", "Wins"]).max().reset_index()[["Team", "Year"]]
-    fin_df = pd.merge(semi_fin_df, filtered_data2, how='left', on=["Team", "Year"])
-    fin_df["Team Name"] = fin_df['Year'].astype(str) + ' ' + fin_df['Team'].astype(str)
-
-    best_chart_figure = px.line(fin_df, x="Game No.", y="Wins", color="Team Name", color_discrete_map=team_color_map)
-    return win_chart_figure, diff_chart_figure, best_chart_figure
-
-
-if __name__ == "__main__":
-    app.run_server(debug=True)
